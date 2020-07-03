bb1925ff3ccf78fcfe3f53b74ba666ad533f91cd
JHblog <1608235427@qq.com>
2020-07-03 15:17:36 +0800

    统一api四层

--- application/route.php
+++ application/route.php
@@ -9,13 +9,15 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
+// return [
+//     '__pattern__' => [
+//         'name' => '\w+',
+//     ],
+//     '[hello]'     => [
+//         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//         ':name' => ['index/hello', ['method' => 'post']],
+//     ],
 
-];
+// ];
+use think\Route;
+Route::rule("hello", "admin/Index/index");
